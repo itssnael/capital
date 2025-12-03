960a53420623f343ffb530eed25bb580b085f627
Alexandra <ardnaxelasweet@gmail.com>
2025-12-03 15:54:01 +0100

    Update mapdata.js

--- mapdata.js
+++ mapdata.js
@@ -22,7 +22,7 @@ var simplemaps_europemap_mapdata={
     location_opacity: 0.8,
     location_hover_opacity: 1,
     location_url: "",
-    location_size: 25,
+    location_size: "7",
     location_type: "square",
     location_image_source: "frog.png",
     location_border_color: "#FFFFFF",
@@ -235,13 +235,215 @@ var simplemaps_europemap_mapdata={
       description: "Kyiv"
     }
   },
+  
   locations: {
     "0": {
       lat: 48.857,
-      lng: 2.341,
+      lng: 2.352,
       name: "Paris"
+    },
+    "1": {
+      lat: 41.327,
+      lng: 19.818,
+      name: "Tirana"
+    },
+    "2": {
+      lat: 40.187,
+      lng: 44.515,
+      name: "Erevan"
+    },
+    "3": {
+      lat: 48.208,
+      lng: 16.373,
+      name: "Vienne"
+    },
+    "4": {
+      lat: 43.856,
+      lng: 18.413,
+      name: "Sarajevo"
+    },
+    "5": {
+      lat: 50.850,
+      lng: 4.351,
+      name: "Bruxelles"
+    },
+    "6": {
+      lat: 42.697,
+      lng: 23.321,
+      name: "Sofia"
+    },
+    "7": {
+      lat: 53.904,
+      lng: 27.561,
+      name: "Minsk"
+    },
+    "8": {
+      lat: 46.948,
+      lng: 7.447,
+      name: "Berne"
+    },
+    "9": {
+      lat: 35.185,
+      lng: 33.382,
+      name: "Nicosie"
+    },
+    "10": {
+      lat: 50.075,
+      lng: 14.437,
+      name: "Prague"
+    },
+    "11": {
+      lat: 52.520,
+      lng: 13.405,
+      name: "Berlin"
+    },
+    "12": {
+      lat: 55.676,
+      lng: 12.568,
+      name: "Copenhague"
+    },
+    "13": {
+      lat: 59.437,
+      lng: 24.753,
+      name: "Tallinn"
+    },
+    "14": {
+      lat: 40.416,
+      lng: -3.703,
+      name: "Madrid"
+    },
+    "15": {
+      lat: 60.169,
+      lng: 24.938,
+      name: "Helsinki"
+    },
+    "16": {
+      lat: 51.507,
+      lng: -0.127,
+      name: "Londres"
+    },
+    "17": {
+      lat: 41.715,
+      lng: 44.827,
+      name: "Tbilissi"
+    },
+    "18": {
+      lat: 37.983,
+      lng: 23.727,
+      name: "Athènes"
+    },
+    "19": {
+      lat: 45.815,
+      lng: 15.981,
+      name: "Zagreb"
+    },
+    "20": {
+      lat: 47.497,
+      lng: 19.040,
+      name: "Budapest"
+    },
+    "21": {
+      lat: 53.349,
+      lng: -6.260,
+      name: "Dublin"
+    },
+    "22": {
+      lat: 64.146,
+      lng: -21.942,
+      name: "Reykjavik"
+    },
+    "23": {
+      lat: 41.902,
+      lng: 12.496,
+      name: "Rome"
+    },
+    "24": {
+      lat: 54.687,
+      lng: 25.279,
+      name: "Vilnius"
+    },
+    "25": {
+      lat: 49.611,
+      lng: 6.131,
+      name: "Luxembourg"
+    },
+    "26": {
+      lat: 56.949,
+      lng: 24.105,
+      name: "Riga"
+    },
+    "27": {
+      lat: 47.010,
+      lng: 28.863,
+      name: "Chisinau"
+    },
+    "28": {
+      lat: 42.430,
+      lng: 19.262,
+      name: "Podgorica"
+    },
+    "29": {
+      lat: 41.998,
+      lng: 21.425,
+      name: "Skopje"
+    },
+    "30": {
+      lat: 52.367,
+      lng: 4.904,
+      name: "Amsterdam"
+    },
+    "31": {
+      lat: 59.913,
+      lng: 10.752,
+      name: "Oslo"
+    },
+    "32": {
+      lat: 52.229,
+      lng: 21.012,
+      name: "Varsovie"
+    },
+    "33": {
+      lat: 38.722,
+      lng: -9.139,
+      name: "Lisbonne"
+    },
+    "34": {
+      lat: 44.426,
+      lng: 26.102,
+      name: "Bucarest"
+    },
+    "35": {
+      lat: 44.786,
+      lng: 20.448,
+      name: "Belgrade"
+    },
+    "36": {
+      lat: 59.329,
+      lng: 18.068,
+      name: "Stockholm"
+    },
+    "37": {
+      lat: 46.056,
+      lng: 14.505,
+      name: "Ljubljana"
+    },
+    "38": {
+      lat: 48.148,
+      lng: 17.107,
+      name: "Bratislava"
+    },
+    "39": {
+      lat: 39.933,
+      lng: 32.859,
+      name: "Ankara"
+    },
+    "40": {
+      lat: 50.450,
+      lng: 30.523,
+      name: "Kyiv"
     }
   },
+  
   regions: {},
   labels: {},
   legend: {
